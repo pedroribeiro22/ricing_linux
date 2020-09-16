12ff16258d8e013b7ff1beb293d2d7687b7b5515
Pedro Ribeiro <pedrouminhoribeiro@gmail.com>
2020-09-16 22:07:22 +0100

    Updates monitor script

--- i3wm/monitor.sh
+++ i3wm/monitor.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
 # Defining which is the main monitor
-MONITOR=eDP1
+MONITOR=eDP-1
 
 # Function that reacts whenever the HDMI1 cable gets plugged
 function ActivateHDMI1 {
     echo "Switching to HDMI1"
-    xrandr --output HDMI1 --mode 1920x1080 --dpi 160 --output eDP1 --off
-    MONITOR=HDMI1
+    xrandr --output HDMI-1 --mode 1920x1080 --dpi 160 --output eDP-1 --off
+    MONITOR=HDMI-1
 }
 
 # Function that reacts whenever the HDMI1 cable gets unplugged
 function DeactivateHDMI1 {
     echo "Switching to eDP1"
-    xrandr --output HDMI1 --off --output eDP1 --auto
-    MONITOR=eDP1
+    xrandr --output HDMI-1 --off --output eDP-1 --auto
+    MONITOR=eDP-1
 }
 
 # Function that checks if HDMI1 cable is connected
