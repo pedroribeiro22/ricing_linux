257b28545da1c37b5d4e668c3bd4fc2fd27104f0
Pedro Ribeiro <pedrouminhoribeiro@gmail.com>
2020-09-16 13:59:36 +0100

    Minor updates

--- alacritty/install.sh
+++ alacritty/install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+path=$(pwd)
+
 [ -d $HOME/.config/alacritty ] && rm -rf $HOME/.config/alacritty
-ln -sf $path/config/alacritty $HOME/.config/alacritty
+ln -sf $path/alacritty $HOME/.config/alacritty
 
